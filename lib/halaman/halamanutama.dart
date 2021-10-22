aae26a9dfa3bebb1d8055d483989539945669bab
aji sasongko <jisong710@gmail.com>
2021-10-23 06:23:03 +0700

    cek

---
+++ lib/halaman/halamanutama.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class halamanutama extends StatefulWidget {
+  halamanutama({Key? key, String title}) : super(key: key);
+
+  @override
+  _halamanutamaState createState() => _halamanutamaState();
+}
+
+class _halamanutamaState extends State<halamanutama> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: ,
+      ),
+    );
+  }
+}
